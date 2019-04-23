7d1624756fe8e2f266bdb43ba7864e68a9c6ee4a
bene802 <wangzetong94@gmail.com>
2019-04-23 15:00:42 -0400

    backend search function completed

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,24 @@
 const express = require("express");
 const app = express();
 const request = require("request");
+const lunr = require("lunr");
 
 app.use(express.json());
 
-const posts = [
-  { id: 1, name: "post 1" },
-  { id: 2, name: "post 2" },
-  { id: 3, name: "post 3" }
-];
-
 const url = "http://jsonplaceholder.typicode.com/posts";
-app.get("/api", (req, res) => {
+
+var posts = [];
+
+app.get("/api/posts", (req, res) => {
+  // error handle, no "title" attribute in query string
+  if (!("title" in req.query)) {
+    return res.status(400).send("Bad Request. No title in query string.");
+  }
+  let query = String(req.query.title);
+  // if user input is empty, return all results.
+  if (!query) {
+    query = "*";
+  }
   request(
     {
       url: url,
@@ -19,25 +26,31 @@ app.get("/api", (req, res) => {
     },
     function(error, response, body) {
       if (!error && response.statusCode === 200) {
-        console.log(body); // Print the json response
-        res.send(body);
-        return;
+        //console.log(body); // Print the json response
+        posts = body.map(a => Object.assign({}, a));
+        const mapPosts = body.reduce(function(map, obj) {
+          map[obj.id] = obj;
+          return map;
+        }, {}); // map <post.id, post>
+        // use lunr to search text in "title"
+        var idx = lunr(function() {
+          this.field("title");
+          this.ref("id");
+          posts.map(post => {
+            this.add(Object.assign({}, post));
+          });
+        });
+        const searchRes = idx.query(function(q) {
+          q.term(query, { fields: ["title"] });
+        });
+        let output = [];
+        searchRes.map(s => {
+          output.push(mapPosts[s.ref]);
+        });
+        return res.send(output);
       }
     }
   );
-  return;
-});
-
-app.get("/api/posts/:id", (req, res) => {
-  const post = post.find(c => c.id === parseInt(req.params.id));
-  if (!post) res.status(404).send("id not find"); // 404 not found
-  res.send(post);
-});
-
-app.put("/api/posts/:id", (req, res) => {
-  // look up the post, if not exist, return 404 not found
-  // validate, if invalid, return 400, bad request
-  // update the post.
 });
 
 const port = process.env.PORT || 3000;
